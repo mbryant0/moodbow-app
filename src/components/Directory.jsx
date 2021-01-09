848a08f2c05c1753887e0ec2b13a91cf514999c5
mikayla-bryant <mikaylabryant25@gmail.com>
2021-01-08 20:04:04 -0500

    Added hover effects to directory cards

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -4,49 +4,52 @@ import DirectoryCard from './DirectoryCard';
 const Directory = () => {
   const [directoryCard, setDirectoryCard] = useState([
     {
-      title: 'Clothing',
+      title: 'clothing',
       image:
         'https://ruthrose.co.uk/wp-content/uploads/2016/04/fashion-pink-story-pastels-editorial-pastel-london-photographer-ruth-rose-sophie-young-storm-solstice-magazine-10-683x1024.jpg',
       imagePosition: '-110px -150px',
       id: '2',
     },
     {
-      title: 'Accessories',
+      title: 'accessories',
       image:
         'https://www.capturefactory.co.uk/wp-content/uploads/2018/12/lifestyle.jpg',
       imagePosition: '-270px -290px',
       id: '4',
     },
     {
-      title: 'Beauty',
+      title: 'beauty',
       image:
         'https://media.istockphoto.com/photos/makeup-brush-and-shiny-sparkles-on-pastel-pink-background-festive-picture-id1166511941?k=6&m=1166511941&s=612x612&w=0&h=-DYeFcWFHoDTaW-WMfvmvW04hKinKo1kiyBx_MKlNOs=',
       imagePosition: '-100px -150px',
       id: '6',
     },
     {
-      title: 'Home & Living',
+      title: 'home & living',
       image:
         'https://cdn.decoist.com/wp-content/uploads/2017/09/Decorating-the-home-office-in-delicate-and-stylish-pastels.jpg',
-      imagePosition: '0px -200px',
+      imagePosition: '0px -220px',
+      size: 'large',
       id: '8',
     },
     {
-      title: "What's New",
+      title: "what's new",
       image:
         'https://expertphotography.com/wp-content/uploads/2018/09/product-photography-checklist-pastel-candy-colour-bag.jpg',
-      imagePosition: '-60px 300px',
+      imagePosition: '-60px 360px',
+      size: 'large',
       id: '10',
     },
   ]);
   return (
     <div className='directory-container'>
-      {directoryCard.map(({ title, image, imagePosition, id }) => {
+      {directoryCard.map(({ title, image, imagePosition, id, size }) => {
         return (
           <DirectoryCard
             image={image}
             key={id}
             title={title}
+            size={size}
             imagePosition={imagePosition}
           />
         );
--- src/components/DirectoryCard.jsx
+++ src/components/DirectoryCard.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './DirectoryCard.scss';
 import './Directory.scss';
 
-const DirectoryCard = ({ title, image, imagePosition }) => {
+const DirectoryCard = ({ title, image, imagePosition, size }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${image})`,
-        backgroundPosition: `${imagePosition}`,
-      }}
-      className='directory-card'
-    >
+    <div className={`${size} directory-card`}>
+      <div
+        className='background-image'
+        style={{
+          width: '100%',
+          height: '100%',
+          backgroundImage: `url(${image})`,
+          backgroundPosition: `${imagePosition}`,
+        }}
+      ></div>
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span className='subtitle'>SHOP NOW</span>
